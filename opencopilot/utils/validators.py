9b0b0e58cc5d0392decf75231d6059af497d8f99
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-08-23 15:48:12 +0200

    Prompt validation

---
+++ opencopilot/utils/validators.py
@@ -0,0 +1,13 @@
+import os
+
+
+def validate_system_prompt(file_path: str):
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as f:
+            prompt = f.read()
+            if not "{question}" in prompt:
+                raise Exception(f"'{{question}}' is missing in '{file_path}' prompt.")
+            if not "{history}" in prompt:
+                raise Exception(f"'{{history}}' is missing in '{file_path}' prompt.")
+    else:
+        raise Exception(f"'{file_path}' is not a file path.")
