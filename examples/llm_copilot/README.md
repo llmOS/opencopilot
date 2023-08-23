d2517f98331f72d123592a6839504ed406be5aff
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-08-23 14:45:50 +0200

    Removed LlamaIndex docs, added scraping script

---
+++ examples/llm_copilot/README.md
@@ -0,0 +1,18 @@
+### Setup
+
+```shell
+pip install -r requirements.txt
+playwright install
+```
+
+### Running
+
+```shell
+python copilot.py
+```
+
+### Re-scraping 
+
+```shell
+python scrape.py
+```
--- examples/llm_copilot/copilot.py
+++ examples/llm_copilot/copilot.py
@@ -9,14 +9,15 @@ from opencopilot import OpenCopilot
 
 load_dotenv()
 
+
 copilot = OpenCopilot(
     prompt_file="prompts/prompt_template.txt",
     copilot_name="llm",
-    auth_type=os.getenv("AUTH_TYPE"),
+    auth_type=os.getenv("AUTH_TYPE") ,
     weaviate_url=os.getenv("WEAVIATE_URL"),
     helicone_api_key=os.getenv("HELICONE_API_KEY"),
-    jwt_client_id=os.getenv("JWT_CLIENT_ID"),
-    jwt_client_secret=os.getenv("JWT_CLIENT_SECRET"),
+    jwt_client_id=os.getenv("JWT_CLIENT_ID") or "",
+    jwt_client_secret=os.getenv("JWT_CLIENT_SECRET") or "",
     jwt_token_expiration_seconds=int(os.getenv("JWT_TOKEN_EXPIRATION_SECONDS") or "0")
 )
 copilot.add_local_files_dir("data")
