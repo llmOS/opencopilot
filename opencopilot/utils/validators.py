39e4ba7133ed01b976e68f58ccd7160a255b6e66
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-08-23 16:05:44 +0200

    Adding 'context' too

--- opencopilot/utils/validators.py
+++ opencopilot/utils/validators.py
@@ -9,5 +9,7 @@ def validate_system_prompt(file_path: str):
                 raise Exception(f"'{{question}}' is missing in '{file_path}' prompt.")
             if not "{history}" in prompt:
                 raise Exception(f"'{{history}}' is missing in '{file_path}' prompt.")
+            if not "{context}" in prompt:
+                raise Exception(f"'{{context}}' is missing in '{file_path}' prompt.")
     else:
         raise Exception(f"'{file_path}' is not a file path.")
