9b2b9baf742fb5759910e741937983d08dd4075e
taivop <taivo@pungas.ee>
2023-08-31 11:08:24 +0300

    refactor: move validation into validators package

--- opencopilot/application.py
+++ opencopilot/application.py
@@ -7,10 +7,9 @@ from typing import Optional
 import uvicorn
 from langchain.schema import Document
 
-from .utils.validators import validate_openai_api_key
+from .utils.validators import validate_openai_api_key, validate_prompt_and_prompt_file_config
 from . import settings
 from .settings import Settings
-from .domain.errors import PromptError
 
 
 class OpenCopilot:
@@ -43,23 +42,12 @@ class OpenCopilot:
         if not openai_api_key:
             openai_api_key = os.getenv("OPENAI_API_KEY")
 
-        if prompt and prompt_file:
-            raise PromptError(
-                "You can only pass either a prompt or a prompt_file argument, not both."
-            )
-        if not prompt and not prompt_file:
-            raise PromptError(
-                "You need to pass either a prompt or a prompt_file argument."
-            )
-        
-        if prompt_file and not os.path.isfile(prompt_file):
-            raise PromptError(
-                f"Prompt file '{prompt_file}' does not exist. Please make sure your prompt file path points to a file that exists."
-            )
+        validate_openai_api_key(openai_api_key)
+        validate_prompt_and_prompt_file_config(prompt, prompt_file)
+
         
         prompt = prompt or open(prompt_file, "r").read()
 
-        validate_openai_api_key(openai_api_key)
 
         settings.set(
             Settings(
--- opencopilot/utils/validators.py
+++ opencopilot/utils/validators.py
@@ -1,6 +1,21 @@
 import os
 from opencopilot.domain.errors import PromptError, APIKeyError
 
+def validate_prompt_and_prompt_file_config(prompt: str, prompt_file: str):
+    if prompt and prompt_file:
+        raise PromptError(
+            "You can only pass either a prompt or a prompt_file argument, not both."
+        )
+    if not prompt and not prompt_file:
+        raise PromptError(
+            "You need to pass either a prompt or a prompt_file argument."
+        )
+    
+    if prompt_file and not os.path.isfile(prompt_file):
+        raise PromptError(
+            f"Prompt file '{prompt_file}' does not exist. Please make sure your prompt file path points to a file that exists."
+        )
+
 
 def validate_system_prompt(prompt: str):
     if not "{question}" in prompt:
