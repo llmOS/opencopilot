a8eb7070b32ff6b03f4f58493b8c412f9257a966
Kaspar Peterson <kasparpeterson@hotmail.com>
2023-09-01 11:39:00 +0300

    Adds auth for e2e_example test.

--- tests/assets/e2e_example.py
+++ tests/assets/e2e_example.py
@@ -1,12 +1,20 @@
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
 from langchain.schema import Document
 
 from opencopilot import OpenCopilot
 
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
 copilot = OpenCopilot(
     prompt_file="tests/assets/e2e_example_prompt.txt",
-    helicone_api_key=os.getenv("HELICONE_API_KEY")
+    helicone_api_key=os.getenv("HELICONE_API_KEY"),
+    auth_type=os.getenv("AUTH_TYPE"),
+    jwt_client_id=os.getenv("JWT_CLIENT_ID"),
+    jwt_client_secret=os.getenv("JWT_CLIENT_SECRET"),
 )
 copilot.add_local_files_dir("tests/assets/e2e_example_data")
 
