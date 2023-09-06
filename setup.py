c70d37958f7fa053916b3b8ecb692c7d0bfa1ecf
Kaspar Peterson <kasparpeterson@hotmail.com>
2023-09-06 16:45:10 +0300

    Release v0.3.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extra_files = package_files('opencopilot')
 
 setup(
     name='opencopilot-ai',
-    version='0.3.4',
+    version='0.3.5',
     packages=["opencopilot"],
     package_data={'': extra_files},
     license="MIT",
