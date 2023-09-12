52d3377030096a1a1842197d582aa9a2647ca0fe
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-09-11 20:23:33 -0700

    Fixes custom LLM streaming

--- opencopilot/domain/chat/results/get_llm.py
+++ opencopilot/domain/chat/results/get_llm.py
@@ -12,13 +12,15 @@ def execute(user_id: str = None, streaming: bool = False) -> BaseChatModel:
     if isinstance(llm, str):
         if settings.get().HELICONE_API_KEY:
             openai.api_base = settings.get().HELICONE_BASE_URL
-        llm = ChatOpenAI(
+        return ChatOpenAI(
             temperature=0.0,
             model_name=settings.get().LLM,
             streaming=streaming,
             model_kwargs={"headers": _get_headers(user_id)},
             openai_api_key=settings.get().OPENAI_API_KEY,
         )
+    if hasattr(llm, "streaming"):
+        llm.streaming = streaming
     return llm
 
 
