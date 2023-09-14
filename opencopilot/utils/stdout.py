c58a7ed20e878d1d469965a8bf66539c80eb4c1d
kristjanpeterson <kristjan@nftport.xyz>
2023-09-14 14:23:17 -0700

    Run linter

--- opencopilot/utils/stdout.py
+++ opencopilot/utils/stdout.py
@@ -3,15 +3,16 @@ import os
 
 def ignore_stdout(func):
     """
-        Wrapper function to not print out stdout and stderr for the function
-        that gets passed here.
-        Usage example:
-        ```
-        @ignore_stdout
-        def my_function(var):
-            return var
-        ```
+    Wrapper function to not print out stdout and stderr for the function
+    that gets passed here.
+    Usage example:
+    ```
+    @ignore_stdout
+    def my_function(var):
+        return var
+    ```
     """
+
     def wrapper(*args, **kwargs):
         # Backup the original file descriptors for stdout and stderr
         original_stdout_fd = os.dup(1)
