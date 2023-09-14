93f83e50571a00e32a292e6f06af05737651dc2d
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-09-14 15:26:02 -0700

    Fixes unit tests

--- tests/unit/service/chat/test_chat_history_service.py
+++ tests/unit/service/chat/test_chat_history_service.py
@@ -1,5 +1,6 @@
 import asyncio
 import uuid
+from unittest.mock import AsyncMock
 from unittest.mock import MagicMock
 
 import pytest
@@ -10,22 +11,12 @@ from opencopilot.service.chat.entities import ChatHistoryRequest
 from opencopilot.service.chat.entities import ChatHistoryResponse
 from opencopilot.service.error_responses import ForbiddenAPIError
 
-MESSAGES = [
-    ChatHistoryItem(
-        content="mock",
-        timestamp=1,
-        response_message_id="rmi"
-    )
-]
+MESSAGES = [ChatHistoryItem(content="mock", timestamp=1, response_message_id="rmi")]
 
 
 def setup_function():
     service.get_chat_history_use_case = MagicMock()
-    service.get_chat_history_use_case.execute.return_value = task_from_result(MESSAGES)
-
-
-async def task_from_result(result):
-    return result
+    service.get_chat_history_use_case.execute = AsyncMock(return_value=MESSAGES)
 
 
 @pytest.mark.asyncio
@@ -47,9 +38,7 @@ async def test_returns_ok():
 
 @pytest.mark.asyncio
 async def test_raises_forbidden():
-    f = asyncio.Future()
-    f.set_result([])
-    service.get_chat_history_use_case.execute.return_value = f
+    service.get_chat_history_use_case.execute = AsyncMock(return_value=[])
 
     conversation_id = uuid.uuid4()
     with pytest.raises(ForbiddenAPIError):
