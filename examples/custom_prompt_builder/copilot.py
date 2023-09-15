07e90cf2abee9d5ec892b63ba5a2587ebdeb111a
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-09-15 15:04:26 -0700

    Adds docstrings

--- examples/custom_prompt_builder/copilot.py
+++ examples/custom_prompt_builder/copilot.py
@@ -21,7 +21,25 @@ copilot = OpenCopilot(prompt=PROMPT)
 
 @copilot.prompt_builder
 def build_prompt(conversation_id: UUID, user_id: str, message: str) -> Optional[str]:
+    """
+    Builds a custom prompt based on the detected user intent.
+
+    This function is decorated with @copilot.prompt_builder which means it will 
+    be used to determine the prompt to pass to the OpenCopilot instance. The 
+    function detects the intent of the incoming message and returns a specific 
+    prompt based on the detected intent.
+
+    Args:
+    - conversation_id (UUID): Unique identifier for the conversation.
+    - user_id (str): Unique identifier for the user.
+    - message (str): The message received from the user.
+
+    Returns:
+    Optional[str]: A custom prompt based on the detected intent or None if 
+                   the copilot should continue without any custom prompting
+    """
     user_intent = intents.detect_intent(message)
+    print(f"Intent {user_intent}")
     if user_intent == Intent.GREETING:
         return 'Say literally "HELLO".'
     return None
