7feac7b72d1ae4e7c16ef407d2fc29cb7989aab3
kristjanpeterson <kristjan@nftport.xyz>
2023-09-19 15:11:07 -0700

    Add basic request headers to url loader

--- opencopilot/utils/loaders/urls_loader.py
+++ opencopilot/utils/loaders/urls_loader.py
@@ -1,12 +1,12 @@
-import os
-import urllib.request
+import tempfile
 from typing import Callable
 from typing import List
 
+import requests
 from langchain.schema import Document
 from langchain.text_splitter import TextSplitter
+from requests import Response
 
-from opencopilot import settings
 from opencopilot.logger import api_logger
 from opencopilot.repository.documents import split_documents_use_case
 from opencopilot.utils.loaders.url_loaders import csv_loader_use_case
@@ -17,13 +17,20 @@ from opencopilot.utils.loaders.url_loaders import xls_loader_use_case
 
 logger = api_logger.get()
 
-loaders: List[Callable[[str, str], List[Document]]] = [
+LOADERS: List[Callable[[str, str], List[Document]]] = [
     pdf_loader_use_case.execute,
     csv_loader_use_case.execute,
     xls_loader_use_case.execute,
     json_loader_use_case.execute,
     html_loader_use_case.execute,
 ]
+
+USER_AGENT: str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"
+CHUNK_SIZE = 5 * 1024 * 1024  # 5MB
+
+
+class FileSizeException(Exception):
+    pass
 
 
 def execute(
@@ -43,28 +50,45 @@ def execute(
 def _load_url(url: str, max_document_size_mb: int) -> List[Document]:
     docs: List[Document] = []
     try:
-        file_name, headers = urllib.request.urlretrieve(url)
-        if (file_size := _get_file_size(file_name)) > max_document_size_mb:
-            logger.warning(
-                f"Document {url} too big ({file_size} > {settings.get().MAX_DOCUMENT_SIZE_MB}), skipping."
+        response = requests.get(url, headers={"User-agent": USER_AGENT})
+        response.raise_for_status()
+
+        with tempfile.NamedTemporaryFile() as temp_file:
+            file_name: str = _download_webpage(
+                response, temp_file, max_document_size_mb
             )
-            return []
-        for loader in loaders:
-            try:
-                new_docs = loader(file_name, url)
-                if new_docs:
-                    docs.extend(new_docs)
-                    break
-            except Exception as e:
-                pass
+            docs.extend(_load_docs_from_file(file_name, url))
+    except FileSizeException as e:
+        logger.warning(f"Document {url} too big, skipping.")
+        return []
     except Exception as e:
-        pass
-    if not docs:
         logger.warning(f"Failed to scrape the contents from {url}")
+        return []
     return docs
 
 
-def _get_file_size(file_path):
-    size_in_bytes = os.path.getsize(file_path)
-    size_in_mb = size_in_bytes / 1024 / 1024
-    return size_in_mb
+def _download_webpage(
+    response: Response,
+    temp_file: tempfile.NamedTemporaryFile,
+    max_document_size_mb: int,
+) -> str:
+    downloaded_size = 0
+    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+        downloaded_size += len(chunk)
+        if downloaded_size > max_document_size_mb * 1024 * 1024:
+            raise FileSizeException()
+        temp_file.write(chunk)
+    return temp_file.name
+
+
+def _load_docs_from_file(file_name: str, url: str) -> List[Document]:
+    docs = []
+    for loader in LOADERS:
+        try:
+            new_docs = loader(file_name, url)
+            if new_docs:
+                docs.extend(new_docs)
+                break
+        except Exception as e:
+            pass
+    return docs
