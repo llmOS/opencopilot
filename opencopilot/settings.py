eeb417d82e44718b2024a7a9faefe7c1a2485add
kristjanpeterson <kristjan@nftport.xyz>
2023-09-25 08:59:35 -0700

    Add utf-8 encoding to settings file reading/writing

--- opencopilot/settings.py
+++ opencopilot/settings.py
@@ -40,7 +40,7 @@ class AppConf:
     def get() -> Optional[Any]:
         try:
             if os.path.exists(CONF_FILE_PATH):
-                with open(CONF_FILE_PATH, "r") as file:
+                with open(CONF_FILE_PATH, "r", encoding="utf-8") as file:
                     value = json.load(file)
                 return AppConf.from_dict(value)
         except:
@@ -50,7 +50,7 @@ class AppConf:
     def save(self):
         try:
             os.makedirs(os.path.dirname(CONF_FILE_PATH), exist_ok=True)
-            with open(CONF_FILE_PATH, "w") as file:
+            with open(CONF_FILE_PATH, "w", encoding="utf-8") as file:
                 json.dump(self.to_dict(), file, indent=4)
         except:
             pass
